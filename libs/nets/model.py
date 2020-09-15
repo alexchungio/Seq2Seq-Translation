4435c1206414f05fc0c16b640b2662279142295b
alexchung <yonganzhong@outlook.com>
2020-09-15 20:23:26 +0800

    add seq2seq model

--- libs/nets/model.py
+++ libs/nets/model.py
@@ -26,7 +26,16 @@ class Encoder(tf.keras.Model):
                                        recurrent_initializer='glorot_uniform')
 
     def __call__(self, input, hidden_state):
+        """
+        :param input:  (batch_size, sequence_length)
+        :param hidden_state: (batch_size, num_units)
+        :return:
+        """
+        # x => (batch_size, sequence length, embedding_dim)
         x = self.embedding(input)
+
+        # output => (batch size, sequence length, units)
+        # state => (batch_size, num_units)
         output, state = self.gru(x, initial_state=hidden_state)
 
         return output, state
@@ -36,6 +45,7 @@ class Encoder(tf.keras.Model):
 
 
 class BahdanauAttention(tf.keras.layers.Layer):
+
     def __init__(self, units):
         super(BahdanauAttention, self).__init__()
         self.W1 = tf.keras.layers.Dense(units)
@@ -43,15 +53,19 @@ class BahdanauAttention(tf.keras.layers.Layer):
         self.V = tf.keras.layers.Dense(1)
 
     def __call__(self, query, values):
-        # query hidden state shape == (batch_size, hidden size)
-        # query_with_time_axis shape == (batch_size, 1, hidden size)
-        # values shape == (batch_size, max_len, hidden size)
+        # query hidden state shape == (batch_size, hidden_size)
+        # query_with_time_axis shape == (batch_size, 1, hidden_size)
+        # values shape == (batch_size, max_len, hidden_size)
         # we are doing this to broadcast addition along the time axis to calculate the score
+        # query_with_time_axis => (batch_size, 1, hidden_size)
         query_with_time_axis = tf.expand_dims(query, axis=1)
 
         # score shape == (batch_size, max_length, 1)
         # we get 1 at the last axis because we are applying score to self.V
         # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+        # self.W1(query_with_time_axis) =>  (batch_size, 1, units)
+        # self.W2(values) =>  (batch_size, max_len, units)
+        # score => (batch_size, max_length, 1)
         score = self.V(tf.nn.tanh(
             self.W1(query_with_time_axis) + self.W2(values)))
 
@@ -66,6 +80,7 @@ class BahdanauAttention(tf.keras.layers.Layer):
 
 
 class Decoder(tf.keras.Model):
+
     def __init__(self, vocab_size, embedding_dim, dec_units, batch_size):
         super(Decoder, self).__init__()
         self.batch_size = batch_size
@@ -81,7 +96,15 @@ class Decoder(tf.keras.Model):
         self.attention = BahdanauAttention(self.dec_units)
 
     def __call__(self, x, hidden, enc_output):
-        # enc_output shape == (batch_size, max_length, hidden_size)
+        """
+
+        :param x: (batch_size, 1)
+        :param hidden: (batch_size, num_units)
+        :param enc_output: (batch_size, max_length, num_units)
+        :return:
+        """
+        # context_vector => (batch_size, num_units)
+        # attention_weights => (batch_size, max_length, 1)
         context_vector, attention_weights = self.attention(hidden, enc_output)
 
         # x shape after passing through embedding == (batch_size, 1, embedding_dim)
@@ -91,6 +114,8 @@ class Decoder(tf.keras.Model):
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
 
         # passing the concatenated vector to the GRU
+        # output => (batch_size, 1, hidden_size)
+        # state => (batch_size, hidden_size)
         output, state = self.gru(x)
 
         # output shape == (batch_size * 1, hidden_size)
